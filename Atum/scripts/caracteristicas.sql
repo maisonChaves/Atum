8f01dfe47548561a129a11c2854ec24493e8053e
maison <maison.chaves@gmail.com>
2013-07-17 17:08:51 -0300

    Scripts de criação das tabelas

---
+++ Atum/scripts/caracteristicas.sql
@@ -0,0 +1,10 @@
+INSERT INTO `caracteristica` (`id`, `descricao`) VALUES (1, 'Input Monetário');
+INSERT INTO `caracteristica` (`id`, `descricao`) VALUES (2, 'Input Data');
+INSERT INTO `caracteristica` (`id`, `descricao`) VALUES (3, 'Input Numerico');
+INSERT INTO `caracteristica` (`id`, `descricao`) VALUES (4, 'Input Email');
+INSERT INTO `caracteristica` (`id`, `descricao`) VALUES (5, 'Input Tel Fixo');
+INSERT INTO `caracteristica` (`id`, `descricao`) VALUES (6, 'Input Cel');
+INSERT INTO `caracteristica` (`id`, `descricao`) VALUES (7, 'Input CPF');
+INSERT INTO `caracteristica` (`id`, `descricao`) VALUES (8, 'Input CNPJ');
+INSERT INTO `caracteristica` (`id`, `descricao`) VALUES (9, 'Input CPF/CNPJ');
+INSERT INTO `caracteristica` (`id`, `descricao`) VALUES (10, 'Input Text');
---
+++ Atum/scripts/tipo.sql
@@ -0,0 +1,2 @@
+INSERT INTO `tipo` (`id`, `descricao`) VALUES (1, 'String');
+INSERT INTO `tipo` (`id`, `descricao`) VALUES (2, 'Integer');
